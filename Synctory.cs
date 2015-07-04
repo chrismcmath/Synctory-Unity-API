7d3af8e09228db444baf44b390df9ce59988d391
Chris McMath <chrismcmath@gmail.com>
2015-07-04 13:02:01 +0800

    Initial commit

---
+++ Synctory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Synctory
+    public class Synctory : MonoBehaviour {
+        public void Start () {
+        
+        }
+        
+        public void Update () {
+        }
+    }
+}
